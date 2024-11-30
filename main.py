4042acaca2493dfa2cf14e1f3d74712c25f22629
Marián Tarageľ <mariantaragel@gmail.com>
2024-11-30 19:57:12 +0100

    Added run script

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ from model import Model
 from view import View
 from controller import Controller
 
+def on_closing():
+    root.quit()
+
 if __name__ == "__main__":
     root = tk.Tk()
+    root.protocol("WM_DELETE_WINDOW", on_closing)
     model = Model()
     controller = Controller(model, None)
     view = View(root, controller)
---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-get install python-tk
+sudo apt install python3-venv
+
+python3 -m venv sfc
+. sfc/bin/activate
+python3 -m pip install -r requirements.txt
+python3 main.py
